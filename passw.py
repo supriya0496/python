08fdeb8a22eeeeccc6ec8694d79ce85a0f19a839
supriya0496 <supriyashankar0496@gmail.com>
2021-11-13 21:03:14 +0530

    new prog password

---
+++ passw.py
@@ -0,0 +1,33 @@
+import sys
+def passs(val):
+   
+    up = 0
+    lo = 0
+    schar = 0
+    digi = 0
+   
+    if len(val)>7:
+       
+       
+        for ele in val:
+            if (ele.islower()):
+                lo+=1
+            if (ele.isupper()):
+                up+=1
+            if (ele.isdigit()):
+                digi+=1
+            if (ele == '@' or ele == '$' or ele == '!'):
+                schar+=1
+       
+    else:
+        assert len(val)>7, 'Password parameter can accept only val greater than 7'
+       
+       
+    if (up>=1 and lo>=1 and digi>=1 and schar>=1):
+        print("Valid password")
+    else:
+        print("Invalid password")
+           
+           
+inp = str(sys.argv[1:])
+passs(inp)
